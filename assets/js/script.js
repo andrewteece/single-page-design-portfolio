038b6f6ff066a7a74cdd01667be9ef563a76ca7b
Andrew Teece <andrewteece@gmail.com>
2024-04-18 15:52:37 -0500

    js for slider

---
+++ assets/js/script.js
@@ -0,0 +1,32 @@
+let img = document.querySelector('.slider img');
+let imgWidth = parseInt(getComputedStyle(img).width);
+let slide = document.querySelector('.slider');
+let slideGap = parseInt(getComputedStyle(slide).gap);
+let step = imgWidth + slideGap;
+let next = document.getElementById('next');
+let previous = document.getElementById('previous');
+let currentSlide = 0;
+
+next.addEventListener('click', function () {
+  currentSlide++;
+  if (currentSlide == 3) {
+    currentSlide = 2;
+    slide.style.transform = `translateX(-${currentSlide * step}px)`;
+  } else if (currentSlide > -1) {
+    slide.style.transform = `translateX(-${currentSlide * step}px)`;
+  } else {
+    slide.style.transform = `translateX(${Math.abs(currentSlide) * step}px)`;
+  }
+});
+
+previous.addEventListener('click', function () {
+  currentSlide--;
+  if (currentSlide == -3) {
+    currentSlide = -2;
+    slide.style.transform = `translateX(-${currentSlide * step}px)`;
+  } else if (currentSlide > -1) {
+    slide.style.transform = `translateX(-${currentSlide * step}px)`;
+  } else {
+    slide.style.transform = `translateX(${Math.abs(currentSlide) * step}px)`;
+  }
+});
